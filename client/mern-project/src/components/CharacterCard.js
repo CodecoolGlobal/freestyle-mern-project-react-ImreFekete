0e5f8498292ef514309ccf3e86a63466c1823f17
Mark Veszelka <120094709+markveszelka@users.noreply.github.com>
2023-05-03 13:44:58 +0200

    design css for charcards ready

--- client/mern-project/src/components/CharacterCard.js
+++ client/mern-project/src/components/CharacterCard.js
@@ -1,29 +1,27 @@
 const CharacterCard = ({ character }) => {
 
     return (
-        <div>
-            <article className='charCard_Wrapper'>
-                <div className='charCard_ImageWrapper'>
-                    <img src={character.image} alt={character.name} />
-                </div>
-                <div className='charCard_ContentWrapper'>
-                    <div className='section'>
-                        {character.name}
-                        <div className='status'>
-                            <p>Status: {character.status}</p>
-                            <p>Species: {character.species}</p>
-                        </div>
-                    </div>
-                    <div className='section'>
-                        <span>Last known location:</span>
-                        {character.location.name}
-                    </div>
-                    <div className='section'>
-                        Placeholder section
+        <article className='charCard_Wrapper'>
+            <div className='charCard_ImageWrapper'>
+                <img className='profilePicture' src={character.image} alt={character.name} />
+            </div>
+            <div className='charCard_ContentWrapper'>
+                <div className='section'>
+                    <h2>{character.name}</h2>
+                    <div className='status'>
+                        <p>Status: {character.status}</p>
+                        <p>Species: {character.species}</p>
                     </div>
                 </div>
-            </article>
-        </div>
+                <div className='section'>
+                    <span>Last known location:</span>
+                    {character.location.name}
+                </div>
+                <div className='section'>
+                    Placeholder section
+                </div>
+            </div>
+        </article>
     )
 }
 
