9151103c58cc573a083a75410625596131181ee1
Mark Veszelka <120094709+markveszelka@users.noreply.github.com>
2023-05-03 16:45:01 +0200

    add loading screen

--- client/mern-project/src/App.js
+++ client/mern-project/src/App.js
@@ -5,6 +5,7 @@ import DisplayCharacters from './components/displayCharacters';
 
 
 function App() {
+  const [isLoaded, setisLoaded] = useState(false);
   const [characters, setCharacters] = useState([]);
   const [filteredChars, setFilteredChars] = useState(null);
   const [favCharacters, setFavCharacters] = useState(null);
@@ -16,6 +17,7 @@ function App() {
         const res = await fetch('http://localhost:3000/api/favchar');
         const data = await res.json();
         setFavCharacters(data);
+        setisLoaded(true);
       } catch (error) {
         console.log(error);
       }
@@ -24,9 +26,7 @@ function App() {
   }, [])
 
   useEffect(() => {
-
     let fetchedCharacters = [];
-
     const fetchPages = async (url) => {
       try {
         const res = await fetch(url)
@@ -46,7 +46,6 @@ function App() {
     fetchPages('https://rickandmortyapi.com/api/character')
   }, []);
 
-
   const handleSearchInputChange = (event) => {
     const searchValue = event.target.value.toLowerCase();
     console.log(event.target.value)
@@ -61,15 +60,25 @@ function App() {
     setAppstate(state);
   }
 
-  return (
-    <div className="App">
-      <Header onClickFavorites={handleFavorites} handleSearchInputChange={handleSearchInputChange} />
-      <br />
-      {appState === 'favorites' ? (favCharacters && <DisplayCharacters characters={favCharacters} displayState={appState} />)
-        :
-        ((filteredChars || characters) && <DisplayCharacters characters={(filteredChars || characters)} displayState={appState} />)}
-    </div>
-  );
+  if (isLoaded) {
+    return (
+      <div className="App">
+        <Header onClickFavorites={handleFavorites} handleSearchInputChange={handleSearchInputChange} />
+        <br />
+        {appState === 'favorites' ? (favCharacters && <DisplayCharacters characters={favCharacters} displayState={appState} />)
+          :
+          ((filteredChars || characters) && <DisplayCharacters characters={(filteredChars || characters)} displayState={appState} />)}
+      </div>
+    );
+  } else {
+    return (
+      <div className="App">
+        <div className="loadingContainer">
+          <img className='loading' alt='loading' src="https://media.tenor.com/BgR83Df82t0AAAAi/portal-rick-and-morty.gif"></img>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
