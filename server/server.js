e5d4a9095bdf7bddd9acee7f8881c05291ac5662
nagmil2077 <68022252+nagmil2077@users.noreply.github.com>
2023-05-04 16:09:32 +0200

    Fix mongoose connection

--- server/server.js
+++ server/server.js
@@ -5,14 +5,12 @@ const userId = 'markveszelka';
 const password = 'iV4cZxpwodOxNYTw';
 const clusterId = 'cluster0.ukekemt';
 const budapestTimeZone = (120 * 60 * 1000);
-mongoose.connect(`mongodb+srv://${userId}:${password}@${clusterId}.mongodb.net/mern-project`);
 
 //EXPRESS:
 const express = require('express');
 const app = express();
 const cors = require('cors');
 const PORT = 3000;
-app.listen(PORT, () => console.log(`http://127.0.0.1:${PORT}`));
 
 app.use(cors());
 app.use(express.json());
@@ -45,3 +43,8 @@ app.delete('/api/favchar/:id', async (req, res) => {
     const allFavChars = await FavChar.find();
     res.status(200).json(allFavChars);
 });
+
+mongoose.connect(`mongodb+srv://${userId}:${password}@${clusterId}.mongodb.net/mern-project`)
+    .then(() => {
+        app.listen(PORT, () => console.log(`http://127.0.0.1:${PORT}`));
+    })
