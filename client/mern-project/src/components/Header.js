f90b76e899f68b1f047423851cfa761f13a0fe0e
Fekete Imre <feketeimi12@gmail.com>
2023-05-18 13:46:42 +0200

    some changes

--- client/mern-project/src/components/Header.js
+++ client/mern-project/src/components/Header.js
@@ -4,16 +4,15 @@ function Header({ appState, onFilterClick, handleSearchInputChange, setCurrentPa
 
     return (
         <div className='headerWrapper'>
-            <div className='headerButtons'>
-                {appState === 'characters' && <button id='favBtn' onClick={() => { setCurrentPage(1); onFilterClick('favCharacters') }}>Favorites</button>}
-                {appState === 'favCharacters' && <button id='mainBtn' onClick={() => { setCurrentPage(1); onFilterClick('characters') }}>Back to Main Page</button>}
-            </div>
             <div className='headers'>
                 <img className='titleImage' title="Go to main page" src={headerPicture} alt='logo' onClick={() => onFilterClick('characters')} />
             </div>
+            <div className='headerButtons'>
+                {appState === 'characters' && <button id='favBtn' onClick={() => { setCurrentPage(1); onFilterClick('favCharacters') }}>Favorites</button>}
+                {appState === 'favCharacters' && <button id='mainBtn' onClick={() => { setCurrentPage(1); onFilterClick('characters') }}>Back to Main Page</button>}
             <div className='searchBar' id="searchBar">
-                <label>Search for a character in our database!</label>
                 <input type="text" placeholder='Search for characters...' onChange={handleSearchInputChange} />
+            </div>
             </div>
         </div>
     );
